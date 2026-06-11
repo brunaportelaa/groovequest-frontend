ab8fa22d2f2d9d935d4be37be493aa8d4ab39c71
brunaportelaa <brunaporteladj@gmail.com>
2026-06-11 17:25:28 -0300

    Design tokens and base CSS for main components created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { DashboardPageComponent } from './features/dashboard/pages/dashboard-page/dashboard-page.component';
+import { AppLayoutComponent } from './layout/app-layout/app-layout.component';
+import { SessionsPageComponent } from './features/sessions/pages/sessions-page/sessions-page.component';
+import { SkillsPageComponent } from './features/skills/pages/skills-page/skills-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppLayoutComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            },
+            {
+                path: 'dashboard',
+                component: DashboardPageComponent
+            },
+            {
+                path: 'sessions',
+                component: SessionsPageComponent
+            },
+            {
+                path: 'skills',
+                component: SkillsPageComponent
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard',
+    }
+];
---
+++ src/app/layout/app-layout/app-layout.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-app-layout',
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
+  templateUrl: './app-layout.component.html',
+  styleUrl: './app-layout.component.scss'
+})
+export class AppLayoutComponent {
+
+}
